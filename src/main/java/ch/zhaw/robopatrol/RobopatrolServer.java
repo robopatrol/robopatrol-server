318db45778441e6d14183bebbe8508eb2a709616
simmljoe <joel.simmler@gmail.com>
2016-04-18 18:18:55 +0200

    Refactor Schedule, Add Waypoint
    Shedule can now handle andy JSON String as attribute. Waypoint is
    inizialized to store multiple x/y coordinates

--- src/main/java/ch/zhaw/robopatrol/RobopatrolServer.java
+++ src/main/java/ch/zhaw/robopatrol/RobopatrolServer.java
@@ -1,6 +1,7 @@
 package ch.zhaw.robopatrol;
 
 import ch.zhaw.robopatrol.res.Schedule;
+import ch.zhaw.robopatrol.res.Waypoint;
 import org.glassfish.jersey.server.ResourceConfig;
 
 /**
@@ -11,7 +12,7 @@ public class RobopatrolServer extends ResourceConfig {
 
     public RobopatrolServer() {
         packages("ch.zhaw.robopatrol.res");
-        registerClasses(Schedule.class);
+        registerClasses(Schedule.class, Waypoint.class);
     }
 
 }
--- src/main/java/ch/zhaw/robopatrol/res/Waypoint.java
+++ src/main/java/ch/zhaw/robopatrol/res/Waypoint.java
@@ -1,17 +1,19 @@
 package ch.zhaw.robopatrol.res;
-import ch.zhaw.robopatrol.*;
+
+import ch.zhaw.robopatrol.XodusDAO;
 import org.json.JSONObject;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
  * Created by Schoggi on 17.04.2016.
- * {"jobname":"someName","attributes":{"someKey":"someValue",[...]}
- * jobname could also be an ID, someValue can be any JSON String
+ * {"name":"someName","wayPoints":{"1":{}"x-val":"int","y-val":"int"}, [...] "n":{[...]}}
+ * name could also be amount of waypoints or an ID. Form of x/y coordinates must be like this!
  */
-@Path("schedule")
-public class Schedule {
+@Path("waypoint")
+public class Waypoint {
     private XodusDAO db = new XodusDAO("Schedule");
 
     @POST
@@ -19,9 +21,9 @@ public class Schedule {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getMessage(String jsonString) {
         JSONObject config = new JSONObject(jsonString);
-        JSONObject schedulerConfig = config.getJSONObject("scheduler_config");
-        db.putToDatabase("jobName", schedulerConfig.getString("jobname"));
-        db.putToDatabase("attributes", schedulerConfig.getString("attributes"));
+        JSONObject wayPoint = config.getJSONObject("name");
+        db.putToDatabase("name", wayPoint.getString("name"));
+        db.putToDatabase("wayPoints", wayPoint.getString("wayPoints"));
         return Response.ok(config, MediaType.APPLICATION_JSON).build();
     }
 
@@ -29,7 +31,7 @@ public class Schedule {
     @Produces(MediaType.APPLICATION_JSON)
     public String getMessage() {
         JSONObject task = new JSONObject();
-        return task.put("jobname", getJsonFromDB()).toString();
+        return task.put("name", getJsonFromDB()).toString();
     }
 
     @GET
@@ -37,7 +39,7 @@ public class Schedule {
     @Path("{key}")
     public String getMessageByKey(@PathParam("key") String key) {
         JSONObject task = new JSONObject();
-        return task.put("jobname", db.getByKey(key)).toString();
+        return task.put("name", db.getByKey(key)).toString();
     }
 
     @DELETE
@@ -49,8 +51,8 @@ public class Schedule {
 
     JSONObject getJsonFromDB(){
         JSONObject name = new JSONObject();
-        name.put("attributes", db.getByKey("attributes"));
-        name.put("jobname", db.getByKey("jobName"));
+        name.put("wayPoints", db.getByKey("wayPooints"));
+        name.put("name", db.getByKey("name"));
         return name;
     }
 
